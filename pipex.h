a7c7defb05dce1c64c979d9c8baea9c27f7227c7
felipe nasser <felipecmnasser@gmail.com>
2024-02-16 03:13:49 -0300

    pipex.h creation

---
+++ pipex.h
@@ -0,0 +1,13 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
+
+#endif
