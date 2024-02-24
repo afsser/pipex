d1cb9a8c35894704251bab650fe1b380b1463b9b
felipe nasser <felipecmnasser@gmail.com>
2024-02-24 01:04:20 -0300

    nao sei

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -5,35 +5,52 @@
 
 int	main(int argc, char **argv)
 {
-	// pid_t	pid;
-	// pid = fork();
-	// if (pid == -1)
-	// {
-	// 	ft_printf("Failed to fork\n");
-	// 	return (1);
-	// }
-	// if (pid > 0)
-	// {
-	// 	ft_printf("This is the Parent Process\n");
-	// }
-	// else
-	// {
-	// 	ft_printf("This is the Child Process\n");
-	// }
+	int		fd[2];
+	pid_t	pid;
+
+	pipe(fd);
+	pid = fork();
+	if (pid == 0)
+	{
+		ft_printf("Child process (PID %d) executing...\n", getpid());
+        sleep(2);
+        ft_printf("Child process ending...\n");
+        sleep(1);
+        exit(42);
+		// close(fd[1]);
+		// dup2(fd[0], 0);
+	}
+	if (pid > 0)
+	{
+		int		status;
+		pid_t	child_pid;
+
+		child_pid = wait(&status);
+		ft_printf("Parent process (PID %d) waiting for child process (PID %d)...\n", getpid(), pid);
+		if (WIFEXITED(status))
+			ft_printf("Child Process (PID %d) killed. Code: %d.\n", child_pid, WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			ft_printf("Child process (PID %d) killed by signal %d.\n", child_pid, WTERMSIG(status));
+        sleep(1);
+		ft_printf("Parent Process Ending\n");
+        sleep(1);
+		// close(fd[0]);
+		// dup2(fd[1], 1);
+	}
+	else
+	{
+		perror("fork error:");
+		return (1);
+	}
 
 
 	char	**args;
 	if (argc == 2) // deve ser 5
 	{
 		args = ft_split(argv[1], ' ');
-		execve("/bin/ls", args, NULL);
+		execve("/bin/ls", args, NULL);  // como atribuir o endereço para o comando correto ??
 	}
-		// write(1, "error", 5);
+		perror("Argument error");
 	
-
-	ft_printf("ta funcionando %s\n", ft_strdup("sim"));
-	ft_printf("5 + 5 = %d\n", ft_strlen("0123456789"));
-	ft_printf("%c\n", ft_toupper('a'));
-	ft_printf("%s\n", ft_strjoin("hello ", "world"));
 	return (0);
 }
