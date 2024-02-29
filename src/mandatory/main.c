d4b1b14e060667473a7bd746b2382f93826896cd
Felipe Caldas Marques Nasser <fcaldas-@c1r2p4.42sp.org.br>
2024-02-29 15:48:06 -0300

    forked, dup2ed, waited and piped from child to parent sucessfully

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: fcaldas- <fcaldas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 16:14:11 by fcaldas-          #+#    #+#             */
-/*   Updated: 2024/02/29 15:47:15 by fcaldas-         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:47:53 by fcaldas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,43 @@
 
 //   -------------  PASSAR ARGUMENTOS (COMANDO) (FLAGS) (AMBIENT VARIABLES)
 
+#define READ_BUFFER_SIZE 100
+
 int	main(int argc, char **argv)
 {
 	int		fd[2];
 	pid_t	pid;
+	char	buffer[READ_BUFFER_SIZE];
+	ssize_t	bytesRead;
 
-	pipe(fd);
+	if (pipe(fd) == -1)
+	{
+		perror("Pipe failed: ");
+		return (0);
+	}
 	pid = fork();
+	if (pid == -1)
+	{
+		perror("Failed to fork\n");
+		return (EXIT_FAILURE);
+	}
 	if (pid == 0)
 	{
-		ft_printf("Child process (PID %d) executing...\n", getpid());
-        sleep(2);
-        ft_printf("Child process ending...\n");
-        sleep(1);
-        exit(42);
-		// close(fd[1]);
-		// dup2(fd[0], 0);
-	}
-	if (pid > 0)
-	{
-		int		status;
-		pid_t	child_pid;
-
-		child_pid = wait(&status);
-		ft_printf("Parent process (PID %d) waiting for child process (PID %d)...\n", getpid(), pid);
-		if (WIFEXITED(status))
-			ft_printf("Child Process (PID %d) killed. Code: %d.\n", child_pid, WEXITSTATUS(status));
-		else if (WIFSIGNALED(status))
-			ft_printf("Child process (PID %d) killed by signal %d.\n", child_pid, WTERMSIG(status));
-        sleep(1);
-		ft_printf("Parent Process Ending\n");
-        sleep(1);
-		// close(fd[0]);
-		// dup2(fd[1], 1);
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		ft_printf("Hi Parent");
+		close(fd[1]);
+		return (EXIT_SUCCESS);
 	}
 	else
 	{
-		perror("fork error:");
-		return (1);
+		close(fd[1]);
+		waitpid(pid, NULL, 0);
+		bytesRead = read(fd[0], buffer, sizeof(buffer) - 1);
+		buffer[bytesRead] = '\0';
+		ft_printf("Message from child: %s\n", buffer);
+		close(fd[0]);
+		return (EXIT_SUCCESS);
 	}
 
 
@@ -59,9 +58,18 @@ int	main(int argc, char **argv)
 	if (argc == 2) // deve ser 5
 	{
 		args = ft_split(argv[1], ' ');
-		execve("/bin/ls", args, NULL);  // como atribuir o endereço para o comando correto ??
+		execve("/bin/ls", args, NULL);
 	}
-		perror("Argument error");
+	else
+	{
+		ft_printf("No args passed\n");
+	}
 	
+	// ft_printf("fd[0] : %d\n", fd[0]);
+	// ft_printf("fd[1] : %d\n", fd[1]);
+	// ft_printf("ta funcionando %s\n", ft_strdup("sim"));
+	// ft_printf("5 + 5 = %d\n", ft_strlen("0123456789"));
+	// ft_printf("%c\n", ft_toupper('a'));
+	// ft_printf("%s\n", ft_strjoin("hello ", "world"));
 	return (0);
 }
